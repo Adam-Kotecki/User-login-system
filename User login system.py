227da4a8d0bdc0b0145d666093f392d29c04d2c9
Adam-Kotecki <a.kotecki11@gmail.com>
2022-12-27 16:34:50 +0100

    Change of layout manager from place() to grid()

--- User login system.py
+++ User login system.py
@@ -10,94 +10,122 @@ from datetime import datetime
 
 root = tk.Tk()
 
-root.geometry('500x400')
+root.geometry('400x400')
 root.resizable(False, False)
 root.title('User login system')
 
-sign_in_btn = tk.Button(root, text="Sign in", command=lambda: login_page())
-sign_in_btn.place(x=160, y=50)
-
-create_account_btn = tk.Button(root, text="Create account", command=lambda: register_page())
-create_account_btn.place(x=160, y=100)
 
 def register_page():
     
     for widgets in root.winfo_children():
         widgets.destroy()
     
-    sign_in_btn = tk.Button(root, text="Sign in", command=lambda: login_page())
-    sign_in_btn.place(x=160, y=330)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=3)
+    root.rowconfigure(1, weight=1)
+    root.rowconfigure(2, weight=2)
+    root.rowconfigure(3, weight=1)
+    root.rowconfigure(4, weight=2)
+    root.rowconfigure(5, weight=1)
+    root.rowconfigure(6, weight=2)
+    root.rowconfigure(7, weight=1)
+    root.rowconfigure(8, weight=2)
+    root.rowconfigure(9, weight=3)
+    root.rowconfigure(10, weight=3)
     
-    label0 = tk.Label(text="", fg = "red")
-    label0.place(x=160 , y=30)
+    label1 = tk.Label(root, text="", fg = "red")
+    label1.grid(column=0, row = 0)
     
-    label1 = tk.Label(text="Username:")
-    label1.place(x=210 , y=80)
-    username = tk.StringVar()
-    textbox1 = tk.Entry(root, width = 30, textvariable = username)
-    textbox1.place(x=160, y=100)
+    label2 = ttk.Label(root, text="Username:")
+    label2.grid(column=0, row = 1)
     
-    label2 = tk.Label(text="Password:")
-    label2.place(x=210 , y=130)
-    password =  tk.StringVar()
-    textbox2 = tk.Entry(root, width = 30, textvariable = password)
-    textbox2.place(x=160, y=150)
+    username = ttk.Entry(root)
+    username.grid(column=0, row = 2,sticky = "N")
     
-    label3 = tk.Label(text="Repeat password:")
-    label3.place(x=210 , y=180)
-    password_again =  tk.StringVar()
-    textbox3 = tk.Entry(root, width = 30, textvariable = password_again)
-    textbox3.place(x=160, y=200)
+    label3 = ttk.Label(root, text="Password:")
+    label3.grid(column=0, row =  3)
     
-    label4 = tk.Label(text="Email:")
-    label4.place(x=210 , y=230)
-    email =  tk.StringVar()
-    textbox4 = tk.Entry(root, width = 30, textvariable = email)
-    textbox4.place(x=160, y=250)
+    password = ttk.Entry(root)
+    password.grid(column=0, row = 4,sticky = "N")
+    
+    label4 = ttk.Label(root, text="Confirm password:")
+    label4.grid(column=0, row =  5)
+    
+    confirm_password = ttk.Entry(root)
+    confirm_password.grid(column=0, row = 6,sticky = "N")
+    
+    label5 = ttk.Label(root, text="Email:")
+    label5.grid(column=0, row = 7)
+    
+    email = ttk.Entry(root)
+    email.grid(column=0, row = 8,sticky = "N")
+    
+    login_button = ttk.Button(root, text="Sign in", command=lambda: login_page())
+    login_button.grid(column=0, row =  10, sticky = "N")
+    
+    register_button = ttk.Button(root, text="Register", command=lambda: send_data())
+    register_button.grid(column=0, row = 9)
     
     def send_data():
         if username.get() == "":
-            label0["text"] = "Please type username"
+            label1["text"] = "Please type username"
         elif (len(username.get()) < 5) or username.get()[0] in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0"):
-            label0["text"] = "Username should consists of at least 5 characters\n and should not starts with digit"
+            label1["text"] = "Username should consists of at least 5 characters\n and should not starts with digit"
         elif password.get() == "":
-            label0["text"] = "Please provide password"
+            label1["text"] = "Please provide password"
         elif (len(password.get()) < 8) or (not any(letter in "!@#$%^&*()-+?_=,<>/" for letter in password.get())):
-            label0["text"] = "Password should be at least 8 characters long and should consists of special character"
-        elif password_again.get() == "":
-            label0["text"] = "Please repeat password"
-        elif password_again.get() != password.get():
-            label0["text"] = "Repeated password does not match password"
+            label1["text"] = "Password should be at least 8 characters long and should consists of special character"
+        elif confirm_password.get() == "":
+            label1["text"] = "Please repeat password"
+        elif confirm_password.get() != password.get():
+            label1["text"] = "Repeated password does not match password"
         elif email.get() == "":
-            label0["text"] = "Please type email"
-            
-    register_btn = tk.Button(root, text="Register", command=lambda: send_data())
-    register_btn.place(x=160, y=290)
+            label1["text"] = "Please type email"
     
+register_page()
+    
+
+def compare_data():
+    pass
+
 def login_page():
     
     for widgets in root.winfo_children():
       widgets.destroy()
     
-    label1 = tk.Label(text="Username:")
-    label1.place(x=210 , y=80)
-    username = tk.StringVar()
-    textbox1 = tk.Entry(root, width = 30, textvariable = username)
-    textbox1.place(x=160, y=100)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=3)
+    root.rowconfigure(1, weight=1)
+    root.rowconfigure(2, weight=2)
+    root.rowconfigure(3, weight=1)
+    root.rowconfigure(4, weight=2)
+    root.rowconfigure(5, weight=6)
+    root.rowconfigure(6, weight=2)
+    root.rowconfigure(7, weight=2)
     
-    label2 = tk.Label(text="Password:")
-    label2.place(x=210 , y=130)
-    password =  tk.StringVar()
-    textbox2 = tk.Entry(root, width = 30, textvariable = password)
-    textbox2.place(x=160, y=150)
+    label1 = tk.Label(root, text="", fg = "red")
+    label1.grid(column=0, row = 0)
     
-    def compare_data():
-        pass
+    label2 = ttk.Label(root, text="Username:")
+    label2.grid(column=0, row = 1)
     
-    login_btn = tk.Button(root, text="Log in", command=lambda: compare_data())
-    login_btn.place(x=160, y=180)
+    username = ttk.Entry(root)
+    username.grid(column=0, row = 2,sticky = "N")
     
-    create_account_btn = tk.Button(root, text="Create account", command=lambda: register_page())
-    create_account_btn.place(x=160, y=210)
+    label3 = ttk.Label(root, text="Password:")
+    label3.grid(column=0, row =  3)
+    
+    password = ttk.Entry(root)
+    password.grid(column=0, row = 4,sticky = "N")
+    
+    label4 = ttk.Label(root, text="")
+    label4.grid(column=0, row =  5)
+    
+    login_button = ttk.Button(root, text="Sign in", command=lambda: compare_data())
+    login_button.grid(column=0, row = 6, sticky = "N")
+    
+    register_button = ttk.Button(root, text="Don't have an account? Register", command=lambda: register_page())
+    register_button.grid(column=0, row =  7, sticky = "N")
+    
 
 root.mainloop()
